e15f21df1e550734f2ee3f1f3dd716cfde0a626f
Stefan Torstensson <openhab.remote@gmail.com>
2019-11-13 15:24:57 +0100

    Fix concurrency problem in FileWriter

--- src/common/logging/file-logger.ts
+++ src/common/logging/file-logger.ts
@@ -1,50 +1,12 @@
 import {getLogLevel, Logger} from "./logging";
 import {LogLevel} from "./log-level";
+import {container} from "@app/ui/ioc";
 
-export class FileLogger extends Logger {
-    private static format(...args: any[]): string {
-        return args.map(arg => {
-            if (typeof arg === "string") {
-                return arg;
-            }
-            if (typeof arg === "number") {
-                return arg.toString();
-            }
-            return JSON.stringify(arg);
-        }).join(" ");
-    }
+export class FileWriter {
 
     private fileHandle: org.tizen.FileStream;
-    private readonly name: string;
 
-    constructor(name: string) {
-        super();
-        this.name = name;
-    }
-
-    debug(...args: any[]): void {
-        this.write(LogLevel.DEBUG, ...args);
-    }
-
-    error(...args: any[]): void {
-        this.write(LogLevel.ERROR, ...args);
-    }
-
-    info(...args: any[]): void {
-        this.write(LogLevel.INFO, ...args);
-    }
-
-    warn(...args: any[]): void {
-        this.write(LogLevel.WARN, ...args);
-    }
-
-    private async write(logLevel: LogLevel, ...text: any[]) {
-        if (logLevel < getLogLevel()) {
-            return;
-        }
-        const level = LogLevel[logLevel];
-        const message = FileLogger.format(`${level}: ${this.name}:`, ...text) + "\n";
-        console.log(message);
+    async write(message: string) {
         if (typeof tizen !== "undefined") {
             const fileStream = await this.getFileStream();
             fileStream.write(message);
@@ -69,6 +31,58 @@ export class FileLogger extends Logger {
                 }, reject);
             }, reject);
         });
+    }
+
+}
+
+export class FileLogger extends Logger {
+    private static format(...args: any[]): string {
+        return args.map(arg => {
+            if (typeof arg === "string") {
+                return arg;
+            }
+            if (typeof arg === "number") {
+                return arg.toString();
+            }
+            return JSON.stringify(arg);
+        }).join(" ");
+    }
+
+    private readonly name: string;
+
+    private readonly fileWriter: FileWriter;
+
+    constructor(name: string) {
+        super();
+        this.name = name;
+        this.fileWriter = container.get(FileWriter);
+    }
+
+    debug(...args: any[]): void {
+        this.write(LogLevel.DEBUG, ...args);
+    }
+
+    error(...args: any[]): void {
+        this.write(LogLevel.ERROR, ...args);
+    }
+
+    info(...args: any[]): void {
+        this.write(LogLevel.INFO, ...args);
+    }
+
+    warn(...args: any[]): void {
+        this.write(LogLevel.WARN, ...args);
+    }
+
+    async write(logLevel: LogLevel, ...text: any[]) {
+        if (logLevel < getLogLevel()) {
+            return;
+        }
+        const timestamp = new Date().toLocaleString("sv-SE");
+        const level = LogLevel[logLevel];
+        const message = FileLogger.format(`${timestamp}  ${level}: ${this.name}:`, ...text) + "\n";
+        console.log(message);
+        await this.fileWriter.write(message);
     }
 
 }
