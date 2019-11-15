79cebb14869f239982c434c4c11e4e408afc72e6
Stefan Torstensson <openhab.remote@gmail.com>
2019-11-15 15:11:24 +0100

    Only tasks requiring Tizen tools should fail when tools are missing

--- grunt/options/shell.js
+++ grunt/options/shell.js
@@ -1,16 +1,31 @@
 const path = require("path");
 
+class TizenTools {
+    constructor(grunt) {
+        this.grunt = grunt;
+        this.win32 = process.platform === "win32";
+    }
+    
+    get tizenHome() {
+        return process.env["TIZEN_HOME"] || grunt.fail.fatal(
+            "TIZEN_HOME must be set to the installation path of Tizen Studio");
+    }
+    
+    get sdb() {
+        return path.join(this.tizenHome, "/tools/sdb");
+    }
+    
+    get tizen() {
+        return path.join(this.tizenHome, `/tools/ide/bin/tizen${this.win32 ? ".bat" : ""}`);
+    }
+}
+
 module.exports = (grunt, options) => {
 
     let debugPort = 0;
-
-    const win32 = process.platform === "win32";
-    const tizenHome = process.env["TIZEN_HOME"];
-
-    const tizenCmd = path.join(tizenHome, `/tools/ide/bin/tizen${win32 ? ".bat" : ""}`);
-    const sdbCmd = path.join(tizenHome, "/tools/sdb");
-
     const buildDir = options.outPath;
+
+    const tools = new TizenTools(grunt);
 
     function tizen() {
         return grunt.config.get("tizen");
@@ -34,15 +49,15 @@ module.exports = (grunt, options) => {
             return withLog("webpack-dev-server --config webpack.dev.js " + envParams(params));
         },
         "tizen-build": {
-            command: `${tizenCmd} build-web -- ${buildDir}`
+            command: () => `${tools.tizen} build-web -- ${buildDir}`
         },
         "tizen-package": {
-            command: cert => `${tizenCmd} package -t wgt -s ${cert} -- ${buildDir}`
+            command: cert => `${tools.tizen} package -t wgt -s ${cert} -- ${buildDir}`
         },
-        install: () => `${sdbCmd} install "${path.join(buildDir, tizen().appName)}.wgt"`,
-        uninstall: () => `${sdbCmd} uninstall ${tizen().package}`,
+        install: () => `${tools.sdb} install "${path.join(buildDir, tizen().appName)}.wgt"`,
+        uninstall: () => `${tools.sdb} uninstall ${tizen().package}`,
         start: {
-            command: () => `${sdbCmd} shell app_launcher -w -s ${tizen().appId}`,
+            command: () => `${tools.sdb} shell app_launcher -w -s ${tizen().appId}`,
             options: {
                 callback: (error, stdout, stderr, done) => {
                     const match = /port:\s?(\d+)/.exec(stdout);
@@ -51,8 +66,8 @@ module.exports = (grunt, options) => {
                 }
             }
         },
-        portForward: localPort => `${sdbCmd} forward --remove-all && ${sdbCmd} forward tcp:${localPort} tcp:${debugPort}`,
-        kill: () => `${sdbCmd} shell app_launcher -k ${tizen().appId}`,
+        portForward: localPort => `${tools.sdb} forward --remove-all && ${tools.sdb} forward tcp:${localPort} tcp:${debugPort}`,
+        kill: () => `${tools.sdb} shell app_launcher -k ${tizen().appId}`,
         test: "mocha-webpack -c"
     }
 };
